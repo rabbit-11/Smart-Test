ac43a1dfa0f17ef15de32e4ff0a05fae8246b646
ItsHeart <172360670@qq.com>
2020-05-28 00:10:00 +0800

    add

---
+++ backend/src/main/java/com/qzgcsfcc/st/service/PaperService.java
@@ -0,0 +1,25 @@
+package com.qzgcsfcc.st.service;
+
+import com.qzgcsfcc.st.model.Paper;
+import com.qzgcsfcc.st.model.param.ApiResult;
+import com.qzgcsfcc.st.model.relationship.PaperQuestion;
+
+/**
+ * @author dxc
+ * @date 2020/5/27 22:18
+ */
+
+public interface PaperService {
+
+    ApiResult add(Paper paper);
+
+    ApiResult edit(Paper paper);
+
+    ApiResult delete(Integer id);
+
+    ApiResult addQuestion(PaperQuestion paperQuestion);
+
+    ApiResult editQuestion(PaperQuestion paperQuestion);
+
+    ApiResult delQuestion(PaperQuestion paperQuestion);
+}
---
+++ backend/src/main/java/com/qzgcsfcc/st/service/impl/PaperServiceImpl.java
@@ -0,0 +1,70 @@
+package com.qzgcsfcc.st.service.impl;
+
+import com.qzgcsfcc.st.dao.PQMapper;
+import com.qzgcsfcc.st.dao.PaperMapper;
+import com.qzgcsfcc.st.model.Paper;
+import com.qzgcsfcc.st.model.param.ApiResult;
+import com.qzgcsfcc.st.model.relationship.PaperQuestion;
+import com.qzgcsfcc.st.service.PaperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author dxc
+ * @since 2020/05/04
+ */
+@Service
+public class PaperServiceImpl implements PaperService {
+
+    @Autowired
+    private PaperMapper paperMapper;
+    @Autowired
+    private PQMapper pqMapper;
+
+    @Override
+    public ApiResult add(Paper paper) {
+        paperMapper.insert(paper);
+        ApiResult apiResult = new ApiResult();
+        return apiResult;
+    }
+
+    @Override
+    public ApiResult edit(Paper paper) {
+        paperMapper.updateByPrimaryKey(paper);
+        ApiResult apiResult = new ApiResult();
+        return apiResult;
+    }
+
+    @Override
+    public ApiResult delete(Integer id) {
+        paperMapper.deleteByPrimaryKey(id);
+        ApiResult apiResult = new ApiResult();
+        return apiResult;
+    }
+
+    @Override
+    public ApiResult addQuestion(PaperQuestion paperQuestion) {
+        pqMapper.insert(paperQuestion);
+        ApiResult apiResult = new ApiResult();
+        return apiResult;
+    }
+
+    @Override
+    public ApiResult editQuestion(PaperQuestion paperQuestion) {
+        pqMapper.updateByPrimaryKey(paperQuestion);
+        ApiResult apiResult = new ApiResult();
+        return apiResult;
+    }
+
+    @Override
+    public ApiResult delQuestion(PaperQuestion paperQuestion) {
+        pqMapper.delete(paperQuestion);
+        ApiResult apiResult = new ApiResult();
+        return apiResult;
+    }
+}
